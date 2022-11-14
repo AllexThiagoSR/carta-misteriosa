3f024e7fa45252537e49a44c628ff97fb3cf0f88
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-14 14:44:49 -0300

    Adiciona classes aleatórias a cada carta gerada

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const creatLetter = document.getElementById('criar-carta');
 const input = document.getElementById('carta-texto');
 const letter = document.getElementById('carta-gerada');
 
+const styles = {
+  style: ['newspaper', 'magazine1', 'magazine2'],
+  height: ['medium', 'big', 'reallybig'],
+  rotation: ['rotateleft', 'rotateright'],
+  inclination: ['skewleft', 'skewright'],
+}
+
 creatLetter.addEventListener('click', () => {
   letter.innerHTML = '';
   if (input.value.match(/[a-z]/gi) === null || input.value === '') {
@@ -10,7 +17,15 @@ creatLetter.addEventListener('click', () => {
     const wordsOfLetter = input.value.split(' ');
 
     for (let word of wordsOfLetter) {
-      letter.insertAdjacentHTML('beforeend', `<span>${word}</span> `);
+      const span = document.createElement('span');
+      const keys = Object.keys(styles);
+      for (let key of keys) {
+        const num = Math.floor(Math.random() * styles[key].length)
+        span.classList.add(styles[key][num]);
+      }
+      span.innerText = word;
+      letter.append(span);
+      letter.innerHTML += ' ';
     }
   }
 });
