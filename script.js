ce9eb68bd40c917dfebe7531ccb17b3dd11caa73
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-14 15:27:33 -0300

    Adiciona o escutador de eventos que modifica o estilo da palavra se ela for clicada

--- script.js
+++ script.js
@@ -9,6 +9,24 @@ const styles = {
   inclination: ['skewleft', 'skewright'],
 };
 
+const addClass = (span) => {
+  span.className = '';
+  const keys = Object.keys(styles);
+  for (let index1 = 0; index1 < keys.length; index1 += 1) {
+    const key = keys[index1];
+    const num = Math.floor(Math.random() * styles[key].length);
+    span.classList.add(styles[key][num]);
+  }
+}
+
+const addEvents = () => {
+  for (let element of letter.children) {
+    element.addEventListener('click', (event) => {
+      addClass(event.target);
+    });
+  }
+}
+
 creatLetter.addEventListener('click', () => {
   letter.innerHTML = '';
   if (input.value.match(/[a-z]/gi) === null || input.value === '') {
@@ -17,15 +35,13 @@ creatLetter.addEventListener('click', () => {
     const wordsOfLetter = input.value.split(' ');
     for (let index = 0; index < wordsOfLetter.length; index += 1) {
       const span = document.createElement('span');
-      const keys = Object.keys(styles);
-      for (let index1 = 0; index1 < keys.length; index1 += 1) {
-        const key = keys[index1];
-        const num = Math.floor(Math.random() * styles[key].length);
-        span.classList.add(styles[key][num]);
-      }
+      
+      addClass(span);
       span.innerText = wordsOfLetter[index];
-      letter.append(span);
+      letter.appendChild(span);
       letter.innerHTML += ' ';
+      console.log(letter.children);
     }
+    addEvents();
   }
 });
